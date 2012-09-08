b17e7e08a2c841b07b01e122532d52dcf4032552
Kan O <zkan.cs@gmail.com>
2012-09-08 07:13:35 +0700

    small mods

--- mlclass-ex1/ex1.m
+++ mlclass-ex1/ex1.m
@@ -56,7 +56,7 @@ X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
 theta = zeros(2, 1); % initialize fitting parameters
 
 % Some gradient descent settings
-iterations = 100;
+iterations = 1500;
 alpha = 0.01;
 
 % compute and display initial cost
